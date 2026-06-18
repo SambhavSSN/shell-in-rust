a1ff5b8bfd94e3d254a5a47fab1fad12a7f74a45
SambhavSSN <swapnasambhavn@gmail.com>
2026-06-18 23:53:06 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    //user input
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).unwrap();
+    println!("{}: command not found", user_input.trim());
 }
