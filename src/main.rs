6da18f4137d722fd9c00d5dc158a311b1a42b066
SambhavSSN <swapnasambhavn@gmail.com>
2026-06-19 20:56:19 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,14 @@ fn main() {
             break;
         }else if user_input.starts_with("echo "){
             println!("{}",&user_input[5..]);
+        }else if user_input.starts_with("type ") {
+            let cmd = &user_input[5..].to_string();
+            if ["echo", "exit", "type"].iter().any(|&s| s == cmd) {
+                println!("{} is a shell builtin",cmd);
+            }
+            else{
+                println!("{}: not found", cmd);
+            }
         }else{
             println!("{}: command not found", user_input.trim());
         }
