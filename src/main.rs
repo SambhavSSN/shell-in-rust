aa8d2f293f94d36bb18a100fe7e78e25cdfe285f
SambhavSSN <swapnasambhavn@gmail.com>
2026-06-19 22:07:19 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
         }else if user_input.starts_with("echo "){
             println!("{}",&user_input[5..]);
         }else if user_input.starts_with("type ") {
-            let cmd = &user_input[5..].to_string();
+            let cmd = &user_input[5..];
             if ["echo", "exit", "type"].iter().any(|&s| s == cmd) {
                 println!("{} is a shell builtin",cmd);
             }
